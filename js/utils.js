546123d2ba57842687743784bc96e92922af9888
Taly <talymoss@gmail.com>
2017-06-04 14:23:36 +0300

    Cleanup the project and added getRandomColor

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function isPrimeNumber(number) {
     if (number < 1) throw new Error("number can not be negative or zero");
     if (number === 1) return false;
@@ -11,3 +13,9 @@ function isPrimeNumber(number) {
     return true;
 }
 
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
